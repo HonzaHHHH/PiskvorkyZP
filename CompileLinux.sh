d55195f47941d5060f74e9f9693d4d0bb5d66917
HonzaHHHH <honzahuml@hotmail.com>
2025-04-17 13:47:25 +0200

    Updated compilation script to include multiplayer and singleplayer files; changed success message.

--- CompileLinux.sh
+++ CompileLinux.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
 mkdir compiledProgram
-gcc main.c piskvorkySettings.c terminalSettings.c -o ./compiledProgram/piskvorky
+gcc main.c piskvorkySettings.c terminalSettings.c multiplayer.c singleplayer.c -o ./compiledProgram/piskvorky
 chmod +x ./compiledProgram/piskvorky
 cp ProgramFileNo1.sh -t ./compiledProgram/
 mv ./compiledProgram/ProgramFileNo1.sh  ./compiledProgram/Start.sh
 chmod +x ./compiledProgram/Start.sh
-echo Kompilace úspěšná
+echo Kompilace hotová
